31649e66d212cdc0141cb481866a732f84c0babb
jayce <jayce@163.com>
2022-01-06 21:57:40 +0800

    insert brand module commit

--- src/main/java/org/test/TestGit.java
+++ src/main/java/org/test/TestGit.java
@@ -5,5 +5,7 @@ public class TestGit {
     public static void main(String[] args) {
 
         System.out.println("finished user moudle!");
+        System.out.println("finished brand moudle!");
+
     }
 }
