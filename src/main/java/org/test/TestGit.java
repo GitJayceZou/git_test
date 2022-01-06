fb46b84d3f496c8ab0ade34b8f17560c0941e775
jayce <jayce@163.com>
2022-01-06 22:17:58 +0800

    modify advertise module commit by hot fix

--- src/main/java/org/test/TestGit.java
+++ src/main/java/org/test/TestGit.java
@@ -7,5 +7,6 @@ public class TestGit {
         System.out.println("finished user moudle!");
         System.out.println("finished brand moudle!");
         System.out.println("finished advertise moudle!");
+        System.out.println("modifyed advertise moudle by hot fix!");
     }
 }
